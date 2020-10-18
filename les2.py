ec179119b931b48b27a59c4928034e4824d61d48
DedaLenya <ip66@bk.ru>
2020-10-18 22:32:48 +0500

    Les2(16.10.2020)HomeWorkTask1-6

---
+++ les2.py
@@ -0,0 +1,52 @@
+var_int = 1
+var_str = 'строка'
+var_bool = True
+var_float = 12.234
+
+"""
+Классы типы данных
+Изменяемые
+Не изменяемы типы данных
+"""
+
+# коллекции итерируемые
+
+
+"""
+var_list = [1,2, 'hello', 'world' True, ] #Список изменяемый
+users = []
+
+while True:
+    user_template = {}
+    user_template ['name'] = input()
+    
+
+
+for item in users:
+    print(item)
+    if item['name'] == 'asdgfs':
+        break
+
+for item in users:
+    if not item['name'].islower():
+        item['name'] = item ['item'].title()
+    print(item)
+print(users)
+"""
+
+# for el in reversed("abrakadbra"):
+#    print(el)
+
+line = list ('5someline6')
+print(line)
+print(type(line))
+
+lineT = (4, 234, 45.8, "text", "word", "el", True, None, -77, False)
+# print(lineT)
+# print(type(lineT))
+
+def my_type(el):
+    for el in range(len(lineT)):
+        print(type(lineT[el]))
+    return
+my_type(lineT)
